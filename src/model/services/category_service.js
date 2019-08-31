54303d4c133fbce77e629f54837724ecd41324b6
MytQwayNode <myt.qway.node@gmail.com>
2019-08-31 15:00:02 -0600

    refactor the category service

--- src/model/services/category_service.js
+++ src/model/services/category_service.js
@@ -1,14 +1,22 @@
 const CB = require('../dao/couchbaseUtil')
+const defs = require('../../common/definitions')
+const Logger = require('../../common/lib/logger')('service/category_service.js')
 
 class CategoryService {
-    constructor () {
+    constructor() {
     }
 
     async getSubCategories(categoryProduct) {
+
         if (categoryProduct) {
-            const subCats = await CB.get(categoryProduct)
-            return subCats
+            const subCategory = defs.subCategory[categoryProduct.toUpperCase()]
+            if (subCategory) {
+                const subCats = await CB.get(subCategory)
+                return subCats
+            }
+            Logger.db('Unexpected category : ' + categoryProduct)
         }
+        Logger.db('Unexpected category : ' + categoryProduct)
     }
 }
 
--- src/routes/category.js
+++ src/routes/category.js
@@ -9,17 +9,17 @@ const router = new Router({
 const defs = require('../common/definitions')
 const { CategoryService } = require('../model/services/category_service')
 
+const Logger = require('../common/lib/logger')('routes/category.js')
 /**
  * return a list of fruits: apple, banana...
  */
 router.get('/category/subCategory/:id', async (ctx, next) => {
-    console.log(ctx.params.id.toUpperCase())
-    console.log('= = = + = = =')
-    // await next()
+    Logger.warn(ctx.params.id.toUpperCase())
+    // FIXME NEED TO CHECK params?
     try {
         const service = new CategoryService()
 
-        const subCategories = await service.getSubCategories(defs.subCategory[ctx.params.id.toUpperCase()])
+        const subCategories = await service.getSubCategories(ctx.params.id)
 
         ctx.status = defs.httpStatus.Created
         ctx.body = {
