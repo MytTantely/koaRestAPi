942c99449fa3ab098cadaafb371e37657adc47aa
MytQwayNode <myt.qway.node@gmail.com>
2019-09-01 12:27:06 -0600

    refactor backend name

---
+++ src/api.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const { CategoryService } = require('./model/services/category_service')
+const { ProductsService } = require('./model/services/products_service')
+
+module.exports = { CategoryService, ProductsService }
